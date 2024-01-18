8dc219bb0eefc0c14785f9dc1ee28b5fdc1f94c0
ogunlade adebayo peter <ogunladeadebayopeter@gmail.com>
2024-01-18 10:11:44 -0100

    new file

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const PORT = 5000
 
 app.set("view engine", "ejs")
 app.use( express.static("public"))
+app.use(express.urlencoded({extended:true}))
+
+let users = [
+    {
+        name:"adebayo",
+        email:"adebayo@gmail.com",
+        password:"123456"
+    },
+    {
+        name:"adebayo",
+        email:"adebayo@gmail.com",
+        password:"123456"
+    }
+]
 
 
 app.get("/", (req, res)=>{
@@ -19,18 +33,24 @@ app.get("/", (req, res)=>{
 })
 
 app.get("/welcome", (req, res)=>{
-    res.render("index");
+    res.render("welcome");
 })
 app.get("/navbar", (req, res)=>{
-    res.render("navBar");
+    res.render("navBar",{users:users});
 })
 app.get("/signup", (req, res)=>{
     res.render("signUp");
+    console.log(req.body);
 })
 app.get("/login", (req, res)=>{
     res.sendFile(__dirname + "/login.html");
 })
-
+app.post("/register",(req, res)=>{
+    console.log(req.body);
+    users.push(req.body)
+    console.log(users);
+    res.redirect("/navBar")
+})
 
 app.listen(PORT, (err)=>{
     if(err){
